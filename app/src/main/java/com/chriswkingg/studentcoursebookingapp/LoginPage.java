f798c703f3defd3d19a9cbfec341d56149e828fb
Faraz Tokhanbeigi <faraz@Farazs-MacBook-Pro.local>
2022-06-12 18:50:47 -0600

    Upaded Log-in Page

---
+++ app/src/main/java/com/chriswkingg/studentcoursebookingapp/LoginPage.java
@@ -0,0 +1,19 @@
+package com.chriswkingg.studentcoursebookingapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+
+        String userType = getIntent().getStringExtra("USERTYPE");
+    }
+}
--- app/src/main/java/com/chriswkingg/studentcoursebookingapp/MainActivity.java
+++ app/src/main/java/com/chriswkingg/studentcoursebookingapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.chriswkingg.studentcoursebookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button adminBtn = (Button) findViewById(R.id.admin);
+        Button teacherBtn = (Button) findViewById(R.id.teacher);
+        Button studentBtn = (Button) findViewById(R.id.student);
+
+
+        adminBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, LoginPage.class).putExtra("USERTYPE", "admin"));
+            }
+        });
+
+        teacherBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, LoginPage.class).putExtra("USERTYPE", "teacher"));
+            }
+        });
+
+        studentBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, LoginPage.class).putExtra("USERTYPE", "student"));
+            }
+        });
     }
 }
