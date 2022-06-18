6da9b0cc75845758d51a7b733dd21fe3791654b6
chriswkingg <christopherking@rogers.com>
2022-06-18 13:23:21 -0400

    Added displaying user list

--- app/src/main/java/com/chriswkingg/studentcoursebookingapp/AdminDelUser.java
+++ app/src/main/java/com/chriswkingg/studentcoursebookingapp/AdminDelUser.java
@@ -1,8 +1,12 @@
 package com.chriswkingg.studentcoursebookingapp;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
 
 public class AdminDelUser extends AppCompatActivity{
 
@@ -10,5 +14,14 @@ public class AdminDelUser extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.delete_user);
+
+        ListView userListView = findViewById(R.id.userListView);
+        Database db = new Database(AdminDelUser.this);
+        ArrayList<User> users = db.getUsers();
+        ArrayList<String> userList = new ArrayList<String>();
+        for(User i : users) {
+            userList.add(i.toString());
+        }
+        userListView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, userList));
     }
 }
