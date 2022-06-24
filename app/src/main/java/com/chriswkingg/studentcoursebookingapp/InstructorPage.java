69dc32c3ca554f51a0e3f3a0e5055131b9103758
mehtameet12 <mehtameet12@gmail.com>
2022-06-24 17:28:44 -0400

    added Page for instructor to view the courses

--- app/src/main/java/com/chriswkingg/studentcoursebookingapp/InstructorPage.java
+++ app/src/main/java/com/chriswkingg/studentcoursebookingapp/InstructorPage.java
@@ -1,7 +1,11 @@
 package com.chriswkingg.studentcoursebookingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,8 +15,16 @@ public class InstructorPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.instructor_page);
+        Button instructorCourse = (Button) findViewById(R.id.viewCourse);
 
         TextView welcomeMessage = (TextView) findViewById(R.id.instructorWelcomeMessage);
-        welcomeMessage.setText("Hello " + getIntent().getStringExtra("username") + " logged in as instructor");
+        welcomeMessage.setText("Welcome " + getIntent().getStringExtra("username") + ", your are logged in as an instructor");
+
+        instructorCourse.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Toast.makeText(InstructorPage.this, "Account created, please sign in" , Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(InstructorPage.this, InstructorViewCourse.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/chriswkingg/studentcoursebookingapp/InstructorViewCourse.java
@@ -0,0 +1,20 @@
+package com.chriswkingg.studentcoursebookingapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InstructorViewCourse extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.instructor_courses);
+
+    }
+}
