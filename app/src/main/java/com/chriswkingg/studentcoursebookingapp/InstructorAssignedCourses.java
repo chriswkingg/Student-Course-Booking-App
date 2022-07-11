6e1ab4a95a3c5fdcf736037c30261878a8dad718
mehtameet12 <mehtameet12@gmail.com>
2022-07-11 18:01:51 -0400

    iu2bfiw

--- app/src/main/java/com/chriswkingg/studentcoursebookingapp/InstructorAssignedCourses.java
+++ app/src/main/java/com/chriswkingg/studentcoursebookingapp/InstructorAssignedCourses.java
@@ -35,20 +35,25 @@ public class InstructorAssignedCourses extends AppCompatActivity {
         Toast.makeText(InstructorAssignedCourses.this, "Hey " + getIntent().getStringExtra("username") + ", here are your courses", Toast.LENGTH_SHORT).show();
         courses = db.getCourses();
         final ArrayList<String> courseList = new ArrayList<String>();
-
+        final ArrayList<String> courseCodeToCheck = new ArrayList<String>();
         for (Course i : courses) {
             if (getIntent().getStringExtra("username").equals(i.getInstructor())) {
                 courseList.add(i.toString());
+                courseCodeToCheck.add(i.getCode());
             }
         }
 
         changeTiming.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 for(int i = 0; i < courseList.size(); i++) {
-                    if (courses.get(i).getCode().equals(courseCode.getText().toString())){
-                        courses.get(i).setTiming(editTiming.toString());
+                    if (courseCodeToCheck.get(i).equals(courseCode.getText().toString())){
+                        Toast.makeText(InstructorAssignedCourses.this,  ", Timing called", Toast.LENGTH_SHORT).show();
+                        courses.get(i).setTiming(editTiming.getText().toString());
                         db.deleteCourse(courses.get(i));
                         db.addCourse(courses.get(i));
+                        courseList.remove(i);
+                        courseList.add(courses.get(i).toString());
+                        updateCourses();
                     }
                 }
             }
@@ -57,18 +62,29 @@ public class InstructorAssignedCourses extends AppCompatActivity {
         changeCapacity.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 for(int i = 0; i < courseList.size(); i++) {
-                    if (courses.get(i).getCode().equals(courseCode.getText().toString())){
-                        Toast.makeText(InstructorAssignedCourses.this,  ", erwerwerew", Toast.LENGTH_SHORT).show();
-                        courses.get(i).setCapacity(editCap.toString());
+                    if (courseCodeToCheck.get(i).equals(courseCode.getText().toString())){
+                        Toast.makeText(InstructorAssignedCourses.this,  ", Cap Called", Toast.LENGTH_SHORT).show();
+                        courses.get(i).setCapacity(editCap.getText().toString());
                         db.deleteCourse(courses.get(i));
                         db.addCourse(courses.get(i));
                         courseList.remove(i);
                         courseList.add(courses.get(i).toString());
+                        updateCourses();
                     }
                 }
             }
         });
         assignedCourses.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, courseList));
     }
+    private void updateCourses() {
+        courses.clear();
+        final Database db = new Database(this);
+        courses = db.getCourses();
+        ArrayList<String> courseList = new ArrayList<String>();
+        for (Course i : courses) {
+            courseList.add(i.toString());
+        }
+        assignedCourses.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, courseList));
 
+    }
 }
