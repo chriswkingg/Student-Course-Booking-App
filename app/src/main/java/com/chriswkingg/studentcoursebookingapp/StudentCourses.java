24e1a052873a8ff1b14844ea1e47adc0a95bec9b
mehtameet12 <mehtameet12@gmail.com>
2022-07-20 19:11:50 -0400

    Student Enroll, search and view works!

--- app/src/main/java/com/chriswkingg/studentcoursebookingapp/StudentCourses.java
+++ app/src/main/java/com/chriswkingg/studentcoursebookingapp/StudentCourses.java
@@ -3,14 +3,28 @@ package com.chriswkingg.studentcoursebookingapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentCourses extends AppCompatActivity {
+    ListView studentCourseList;
+    List<String> listOfCourses;
+    List<String> listOfTiming;
+    List<String> combinedList = new ArrayList<>();
+    TextView welcomeMessage;
+    StudentEnroll ob = new StudentEnroll();
+
+    private static ArrayList<String> courseList;
+    private static ArrayList<String> courseTiming;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +32,41 @@ public class StudentCourses extends AppCompatActivity {
         setContentView(R.layout.student_mycourses);
         Button unEnroll = (Button) findViewById(R.id.unEnroll);
         EditText courseCode = (EditText) findViewById(R.id.StudentUnEnrollCode);
+        welcomeMessage = (TextView) findViewById(R.id.studentCourseMess);
+        welcomeMessage.setText("Hey " + getIntent().getStringExtra("username") + ", here are your courses");
+        studentCourseList = this.findViewById(R.id.studentMyCourseList);
+        updateCourses();
+
+    }
+    private void updateCourses() {
+
+        listOfCourses = ob.getMyCourses();
+        listOfTiming = ob.getMyCourseTiming();
+        Toast.makeText(StudentCourses.this, "ListTiming Size is  "+listOfTiming.size() , Toast.LENGTH_SHORT).show();
+
+
+        courseList = new ArrayList<String>();
+        courseTiming = new ArrayList<String>();
+
+
+        if (listOfCourses == null){
+            welcomeMessage = (TextView) findViewById(R.id.studentCourseMess);
+            welcomeMessage.setText( getIntent().getStringExtra("username") + ", you are not enrolled in any classes");
+        }else {
+            for (String i : listOfCourses) {
+                courseList.add(i);
+            }
+            for(String i: listOfTiming){
+                courseTiming.add(i);
+            }
+//            combinedList.addAll(courseList);
+//            combinedList.addAll(courseTiming);
+            for (int i=0; i<courseTiming.size(); i++){
+                combinedList.add("Course: "+listOfCourses.get(i) +", Timing: "+ listOfTiming.get(i));
+            }
+        }
+
+        studentCourseList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, combinedList));
 
     }
 }
--- app/src/main/java/com/chriswkingg/studentcoursebookingapp/StudentEnroll.java
+++ app/src/main/java/com/chriswkingg/studentcoursebookingapp/StudentEnroll.java
@@ -18,27 +18,78 @@ public class StudentEnroll extends AppCompatActivity {
     ListView studentCourseList;
     Database db;
     ArrayList<Course> listOfCourses;
+    int idx = -1;
+    private static ArrayList<String> myCourses;
+    private static ArrayList<String> myCourseTiming;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student_enroll);
         Button studentEnroll = (Button) findViewById(R.id.studentEnrollCourse);
-        EditText courseCode = (EditText) findViewById(R.id.studentCourseCode);
+        Button studentSearchCourse = (Button) findViewById(R.id.studentSearchCourse);
+
+        final EditText courseCode = (EditText) findViewById(R.id.studentCourseCode);
+        final EditText courseName = (EditText) findViewById(R.id.studentCourseName);
+        final EditText courseTiming = (EditText) findViewById(R.id.studentCourseTiming);
+
         studentCourseList = this.findViewById(R.id.studentCourseListView);
         db = new Database(StudentEnroll.this);
         listOfCourses = db.getCourses();
         updateCourses();
 
+        studentSearchCourse.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                idx = -1;
+                for(int i = 0; i < listOfCourses.size(); i++) {
+                    if ((listOfCourses.get(i).getCode().equals(courseCode.getText().toString())) || (listOfCourses.get(i).getName().equals(courseName.getText().toString())) || (listOfCourses.get(i).getTiming().equals(courseTiming.getText().toString())) ) {
+                        idx = i;
+                    }
+                }
+                if (idx==-1){
+                    Toast.makeText(StudentEnroll.this, "Sorry, couldn't find course." , Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(StudentEnroll.this, "Course is available, click Enroll" , Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
         studentEnroll.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Toast.makeText(StudentEnroll.this,  "You have Successfully Enrolled in the course!", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(StudentEnroll.this, StudentPage.class);
-                intent.putExtra("username", getIntent().getStringExtra("username"));
-                startActivity(intent);
+                int flag = -1;
+                for(int i = 0; i < listOfCourses.size(); i++) {
+                    if ((listOfCourses.get(i).getCode().equals(courseCode.getText().toString())) || (listOfCourses.get(i).getName().equals(courseName.getText().toString())) || (listOfCourses.get(i).getTiming().equals(courseTiming.getText().toString())) ) {
+                        flag = i;
+                        if (myCourses == null && myCourseTiming == null){
+                            myCourses = new ArrayList<String>();
+                            myCourseTiming = new ArrayList<String>();
+                        }
+                        if (listOfCourses.get(i).getCode().equals(courseCode.getText().toString()) ){
+                            myCourses.add(listOfCourses.get(i).getCode());
+                            myCourseTiming.add(listOfCourses.get(i).getTiming());
+                            Toast.makeText(StudentEnroll.this, "You have Successfully Enrolled in the course!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(StudentEnroll.this, StudentPage.class);
+                            intent.putExtra("username", getIntent().getStringExtra("username"));
+                            startActivity(intent);
+                        }
+                    }
+                }
+                if (flag == -1){
+                    Toast.makeText(StudentEnroll.this,  "No such course found!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
     }
+    public static ArrayList<String> getMyCourses(){
+        return myCourses;
+    }
+
+    public static ArrayList<String> getMyCourseTiming(){
+        return myCourseTiming;
+    }
+
     private void updateCourses() {
         listOfCourses.clear();
         listOfCourses = db.getCourses();
